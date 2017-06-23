dfcc97b3c3154130c8e26e52a4c8ce6b31c8996c
Gjchiu <george33669@gmail.com>
2017-06-23 20:24:34 +0800

    aaaaa

---
+++ src/com/order/controller/Store_OrderServlet.java
@@ -0,0 +1,61 @@
+package com.order.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.order.model.Store_OrderDAO;
+import com.order.model.Store_OrderVO;
+
+@SuppressWarnings("serial")
+@WebServlet("/Store_OrderServlet1")
+public class Store_OrderServlet extends HttpServlet{
+	private final static String CONTENT_TYPE = "text/html; charset=UTF-8";
+ 
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Store_OrderDAO orderDao = new Store_OrderDAO();
+		List<Store_OrderVO> orderList = orderDao.getAll();
+		writeText(response, new Gson().toJson(orderList));
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		Gson gson = new Gson();
+		BufferedReader br = request.getReader();
+		StringBuilder jsonIn = new StringBuilder();
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			jsonIn.append(line);
+		}
+		JsonObject jsonObject = gson.fromJson(jsonIn.toString(),
+				JsonObject.class);
+		Store_OrderDAO orderDao = new Store_OrderDAO();
+		String action = jsonObject.get("action").getAsString();
+		System.out.println("action: " + action);
+
+		if (action.equals("getAll")) {
+			List<Store_OrderVO> orderList = orderDao.getAll();
+			writeText(response, gson.toJson(orderList));
+		}
+	}
+
+	private void writeText(HttpServletResponse response, String outText)
+			throws IOException {
+		response.setContentType(CONTENT_TYPE);
+		PrintWriter out = response.getWriter();
+		System.out.println("outText: " + outText);
+		out.print(outText);
+	}
+	
+}
